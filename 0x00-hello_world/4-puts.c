b9757e017833804108053ddca5f4a1aae14820fc
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-16 14:01:17 -0800

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* Entry point */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
