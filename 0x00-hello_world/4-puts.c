27e1b7feee70b8562287272372d4310947b1fc97
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-16 14:04:08 -0800

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-/* Entry point */
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	/* print string into stdout */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
