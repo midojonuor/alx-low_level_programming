2a0fe2f4391c9aba0788bd900dc7271e31543468
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-17 05:25:32 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int iter = 0;
+
+	for (iter = 0; iter < 10; ++iter)
+	{
+		putchar('0' + iter);
+	}
+	putchar('\n');
+	return (0);
+}
