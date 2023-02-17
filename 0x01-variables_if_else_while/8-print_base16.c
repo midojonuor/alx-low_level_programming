1ef04d53a91cd87fe84987ca34add7642b122856
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-17 05:37:27 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int iter = 0;
+
+	for (iter = 0; iter < 26; ++iter)
+	{
+		if ( iter > 9)
+		{
+			putchar('a' + iter - 10);
+		}
+		else
+		{
+			putchar('0' + iter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
