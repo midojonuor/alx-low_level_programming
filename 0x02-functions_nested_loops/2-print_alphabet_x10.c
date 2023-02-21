4ddb28a8141f0a4afd99386d3ba1faf4054f6e56
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-21 12:45:37 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_alphabet - function that prints the alphabet
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 26; ++i)
+	{
+		_putchar('a' + i);
+	}
+	_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - function that prints the alphabet
  * Description: 'the program's description'
  * Return: Always 0 (Success)
