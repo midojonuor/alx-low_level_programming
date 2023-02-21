f933b88c4d0b9438e883e21c8d42cf9d3702efb9
midojonuor <mohammedelsayed.mes@gmail.com>
2023-02-21 12:40:13 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define _MAIN_H_
 
 
-export void _putchar(char);
-export void print_alphabet(void);
-export void print_alphabet_x10(void);
+void _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
