ba72c0aa1c1ce5b524cfeae6041dc3883528576f
Mido <midojonuor@outlook.com>
2023-02-28 00:04:57 +0200

    Update solution of 4 in 0x05. C - Pointers, arrays and strings
    C

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, index = 0;
 
-	while (s[index])
+	while (s[index] != '\0')
 	{
 		index++;
 	}
 
 	len = index;
-	
+
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
