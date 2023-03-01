cb451c877afc119d102eac193a8e6f45695e2e0d
midojonuor <mohammedelsayed.mes@gmail.com>
2023-03-01 02:04:59 -0800

    Update solution of 0 in 0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,37 +9,36 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_size = 0;
-    int src_size = 0;
+	int dest_size = 0;
+	int src_size = 0;
 	int concStr_size = 0;
-    int index = 0;
-    char *concStr = NULL;
-    
-    while (dest[dest_size] != '\0')
-    {
-        dest_size += 1;
-    }
-	
-    while (src[src_size] != '\0')
-    {
-        src_size += 1;
-    }
+	int index = 0;
+	char *concStr = NULL;
+
+	while (dest[dest_size] != '\0')
+	{
+		dest_size += 1;
+	}
+
+	while (src[src_size] != '\0')
+	{
+		src_size += 1;
+	}
 
 	concStr_size = dest_size + src_size;
-    
-    concStr = (char *) malloc(sizeof(char) * concStr_size);
+	concStr = (char *) malloc(sizeof(char) * concStr_size);
 
-    for (index = 0; index < dest_size; ++index)
-    {
-        concStr[index] = dest[index];
-    }
-    
-    for (index = dest_size; index < concStr_size; ++index)
-    {
-        concStr[index] = src[index - dest_size];
-    }
+	for (index = 0; index < dest_size; ++index)
+	{
+		concStr[index] = dest[index];
+	}
+
+	for (index = dest_size; index < concStr_size; ++index)
+	{
+		concStr[index] = src[index - dest_size];
+	}
 
 	concStr[concStr_size - 1] = '\0';
 
-    return (concStr);
+	return (concStr);
 }
