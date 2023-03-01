f183888762495fb55abd7ecf3e37939f5f2eb201
Mido <midojonuor@outlook.com>
2023-03-01 11:32:13 +0200

    Solution of 0 in 0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: pointer to destnation
+ * @src: pointer to source
+ * Return: pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+    int dest_size = 0;
+    int src_size = 0;
+    int index = 0;
+    char *concStr = NULL;
+
+    while (dest[dest_size] != '\0')
+    {
+        dest_size += 1;
+    }
+
+    while (src[src_size] != '\0')
+    {
+        src_size += 1;
+    }
+
+    concStr = (char *) malloc(sizeof(char) * (dest_size + src_size - 1));
+
+    for (index = 0; index < (dest_size - 1); ++index)
+    {
+        concStr[index] = dest[index];
+    }
+    
+    for (index = 0; index < (src_size - 1); ++index)
+    {
+        concStr[index] = src[index];
+    }
+
+    return concStr;
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
