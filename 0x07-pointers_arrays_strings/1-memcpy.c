f0bd44bcf210f0fa1823d65297692a193d2943bc
Mido <midojonuor@outlook.com>
2023-03-07 23:59:58 +0200

    Solution of 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
