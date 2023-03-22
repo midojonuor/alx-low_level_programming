eba09b11b89f0a803ed0dfec0c853da6e3527db8
Mido <midojonuor@outlook.com>
2023-03-22 23:28:19 +0200

    Solution of 0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
